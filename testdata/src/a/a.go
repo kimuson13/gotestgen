101f4ba3bc7bff431f287ddc0947bd0978f59e6a
kimuson <kimuson@jxpress.net>
2022-02-09 22:33:08 +0900

    テスト追加(バグ発見)

--- testdata/src/a/a.go
+++ testdata/src/a/a.go
@@ -1,5 +1,39 @@
 package a
 
+import (
+	"database/sql"
+	"fmt"
+)
+
 func hoge(val int) int {
 	return 0
+}
+
+type DB interface {
+	Get(id string) int
+	Insert(val string) error
+}
+
+type db struct {
+	db sql.DB
+}
+
+func (db) Get(id string) int {
+	return 0
+}
+
+func (db) Insert(val string) error {
+	return nil
+}
+
+func helloWorld() {
+	fmt.Println("hello world")
+}
+
+func twoVal() (int, int) {
+	return 0, 0
+}
+
+func nameVal() (result int) {
+	return
 }
